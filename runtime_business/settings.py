05e15254ebe6abffcd61fefd468cd9ad71fc2c8f
Divya <divya@antino.io>
2022-09-12 21:47:28 +0530

    updated settings

--- runtime_business/settings.py
+++ runtime_business/settings.py
@@ -156,11 +156,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # simple JWT settings
